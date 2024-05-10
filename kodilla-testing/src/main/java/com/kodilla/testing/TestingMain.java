6037d1f060c946c70ab844d543d23b6502a94208
unknown <twardym78@gmail.com>
2024-05-10 07:43:59 +0200

    6.2 - tdd new calculator program

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -5,18 +5,21 @@ import com.kodilla.testing.calculator.Calculator;
 public class TestingMain {
     public static void main(String[] args) {
 
-        Calculator numbers = new Calculator(25,14);
+        Calculator numbers = new Calculator(10,5);
 
         double addResult = numbers.addAB();
+        System.out.println(addResult);
         double substractResult = numbers.substractAB();
+        System.out.println(substractResult);
 
-        if (addResult == (numbers.addAB())){
+
+        if (addResult == (15)){
             System.out.println(" Numbers add test -  OK!!");
         } else {
             System.out.println("Error !!!");
         }
 
-        if (substractResult == (numbers.substractAB())){
+        if (substractResult == (5)){
             System.out.println(" Numbers substract test -  OK!!");
         } else {
             System.out.println("Error !!!");
